04d750d427b99e277710a4ee00fab6f95d670172
Puneeth M <puneetm12345@gmail.com>
2025-02-27 17:15:48 +0530

    modified files

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function beforeSubmit() {
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
-  console.log("inputdate.value", inputdate.value); //string --> date (en_IN)
+let capthaChecked = false;
+function beforeSubmit(event) {
+  if (capthaChecked) {
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate.value", inputdate.value); //string --> date (en_IN)
 
-  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-  outputdate.value = formattedDate;
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formattedDate;
+  } else {
+    alert("Please check reCAPTHA box before submitting the lead..");
+    event.preventDefault();
+  }
 }
 
 function timestamp() {
@@ -19,3 +25,7 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function captchaSuccess() {
+  capthaChecked = true;
+}
